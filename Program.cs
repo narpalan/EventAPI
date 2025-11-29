3fd812a2b0334b88df5f0803be1d71fe5bc7bf5c
narpalan <thiagofumega@gmail.com>
2025-11-29 00:06:47 -0300

    chore: configure application startup and development settings

---
+++ Program.cs
@@ -0,0 +1,54 @@
+using Microsoft.EntityFrameworkCore;
+using EventAPI.Data;
+using EventAPI.Services;
+using EventAPI.Validators;
+using FluentValidation;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<EventContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        new MySqlServerVersion(new Version(11, 4, 0))
+    )
+);
+
+// AutoMapper
+builder.Services.AddAutoMapper(typeof(Program));
+
+// FluentValidation
+builder.Services.AddValidatorsFromAssemblyContaining<CreateEventValidator>();
+
+// Services
+builder.Services.AddScoped<IEventService, EventService>();
+
+// Logging
+builder.Services.AddLogging();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+// Create database and tables
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<EventContext>();
+    db.Database.EnsureCreated();
+}
+
+app.Run();
