00dd2e828107bb850ba31308f1145d21d4014783
narpalan <thiagofumega@gmail.com>
2025-11-30 18:40:00 -0300

    :sparkles: feat: create generic IRepository

---
+++ Data/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+using System.Linq.Expressions;
+
+namespace EventAPI.Data.Repositories
+{
+    public interface IRepository<T> where T: class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(int id);
+        Task<T> AddAsync(T entity);
+        Task<T?> UpdateAsync(T entity);
+        Task<bool> DeleteAsync(int id);
+        Task<bool> ExistsAsync(int id);
+    }
+}
