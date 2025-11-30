145d4a2a7253d2da2580d9cda7ff9a2bf877819e
narpalan <thiagofumega@gmail.com>
2025-11-30 18:40:55 -0300

    :recycle: refactor: update IEventRepository with new generic IRepository

--- Data/Repositories/IEventRepository.cs
+++ Data/Repositories/IEventRepository.cs
@@ -3,14 +3,8 @@ using EventAPI.DTOs;
 
 namespace EventAPI.Data.Repositories
 {
-    public interface IEventRepository
+    public interface IEventRepository: IRepository<Event>
     {
-        Task<IEnumerable<Event>> GetAllAsync();
-        Task<Event?> GetByIdAsync(int id);
-        Task<Event> AddAsync(Event eventEntity);
-        Task<Event?> UpdateAsync(Event eventEntity);
-        Task<bool> DeleteAsync(int id);
-        Task<IEnumerable<Event>> GetNearbyEventsAsync(decimal latitude, decimal longitude, double radiusKm);
-        Task<bool> ExistsAsync(int id);
+        Task<IEnumerable<Event>> GetNearbyEventsAsync(decimal latitude, decimal longitude, double radiusKm);        
     }
 }
