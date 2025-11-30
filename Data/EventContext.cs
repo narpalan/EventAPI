81f8309117706c8565897a3cdee3ea3c48848361
narpalan <thiagofumega@gmail.com>
2025-11-30 17:37:36 -0300

    :recycle: refactor: update EventContext to use EventConfiguration on model creation (override done OnModelCreating)

--- Data/EventContext.cs
+++ Data/EventContext.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using EventAPI.Models;
+using EventAPI.Data.Configuration;
 
 namespace EventAPI.Data
 {
@@ -8,5 +9,12 @@ namespace EventAPI.Data
         public EventContext(DbContextOptions<EventContext> options) : base(options) { }
 
         public DbSet<Event> Events => Set<Event>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new EventConfiguration());
+        }
     }
 }
