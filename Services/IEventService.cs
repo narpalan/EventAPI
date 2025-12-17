4c51d3c25ae92e8a9365911cd06b72de9c177782
narpalan <thiagofumega@gmail.com>
2025-12-17 17:48:39 -0300

    :recycle: refactor: removes IService implementation.
    - All necessary methods are now explicitly declared

--- Services/IEventService.cs
+++ Services/IEventService.cs
@@ -2,8 +2,14 @@ using EventAPI.DTOs;
 
 namespace EventAPI.Services
 {
-    public interface IEventService : IService<EventDto, CreateEventDto, UpdateEventDto>
+    public interface IEventService 
     {     
+        Task<IEnumerable<EventDto>> GetAllAsync();
+        Task<EventDto?> GetByIdAsync(int id);
+        Task<EventDto> CreateAsync(CreateEventDto createDto);
+        Task<EventDto?> UpdateAsync(int id, UpdateEventDto updateDto);
+        Task<bool> DeleteAsync(int id);
         Task<IEnumerable<EventDto>> GetNearbyEventsAsync(decimal latitude, decimal longitude, decimal radiusKm);
+
     }
 }
