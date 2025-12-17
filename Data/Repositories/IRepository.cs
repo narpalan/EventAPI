0eccf6ba3c49cb169cb92fbaba074ac33ac3c8a3
narpalan <thiagofumega@gmail.com>
2025-12-17 19:39:52 -0300

    :recycle: refactor: segregates IRepository into smaller intarfaces for ISP compliance
    - Create IReadableRepository, IWritableRepository as a minimum required for new repositories

--- Data/Repositories/IRepository.cs
+++ Data/Repositories/IRepository.cs
@@ -1,12 +1,20 @@
 namespace EventAPI.Data.Repositories
 {
     public interface IRepository<T> where T: class
+    {        
+        Task<bool> ExistsAsync(int id);
+    }
+
+    public interface IReadableRepository<T> where T : class
     {
         Task<IEnumerable<T>> GetAllAsync();
         Task<T?> GetByIdAsync(int id);
+    }
+
+    public interface IWritableRepository<T> where T : class
+    {
         Task<T> AddAsync(T entity);
         Task<T?> UpdateAsync(T entity);
         Task<bool> DeleteAsync(int id);
-        Task<bool> ExistsAsync(int id);
     }
 }
